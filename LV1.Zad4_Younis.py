8cdf72c461d5cdbd6dd4a25917bb8d5646606077
OlayYo <olay.younis18@gmail.com>
2026-03-18 22:17:35 +0100

    Refactor file handling and calculate average confidence

--- LV1.Zad4_Younis.py
+++ LV1.Zad4_Younis.py
@@ -1,7 +1,27 @@
-ime_datoteke = input("Unesi ime datoteke: ")
+naziv = input("Ime datoteke: ")
 
 try:
-    f.open(ime_datoteke)
-except FileNotFoundError:
-    print("Greska (datoteka ne postoji)")
-    exit()
+    fhand = open(naziv)
+except:
+    print("Greška: datoteka", naziv, "nije pronađena.")
+    quit()
+
+ukupno = 0.0
+broj = 0
+
+for linija in fhand:
+    linija = linija.rstrip()
+
+    if linija.startswith("X-DSPAM-Confidence:"):
+        dijelovi = linija.split()
+        vrijednost = float(dijelovi[1])
+        ukupno = ukupno + vrijednost
+        broj = broj + 1
+
+fhand.close()
+
+if broj > 0:
+    prosjek = ukupno / broj
+    print("Average X-DSPAM-Confidence:", prosjek)
+else:
+    print("Nisu pronađene X-DSPAM-Confidence linije u datoteci.")
